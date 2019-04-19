dfb23cfb5d96f607459c9a0673a048357eecc360
Jackson Ming Hu <huming2207@gmail.com>
2019-04-19 11:05:55 +1000

    main: implement demo (and now it works)

--- main/esp_lvgl_main.c
+++ main/esp_lvgl_main.c
@@ -52,11 +52,38 @@ void app_main()
     ESP_LOGI(TAG, "Registering LVGL driver");
     lv_disp_drv_register(&disp);
 
+
     ESP_LOGI(TAG, "Registering LVGL tick callback");
     esp_register_freertos_tick_hook(lv_tick_cb);
 
     ESP_LOGI(TAG, "Starting LVGL main task");
     xTaskCreate(lvgl_main_task, "lv_task", 4096, NULL, 5, NULL);
 
+    lv_obj_t * btn = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 100, 50);                          /*Set its size*/
+
+    lv_btn_set_action(btn, LV_BTN_ACTION_CLICK, NULL);/*Assign a callback to the button*/
+    lv_obj_t * label = lv_label_create(btn, NULL);          /*Add a label to the button*/
+    lv_label_set_text(label, "Button");                     /*Set the labels text*/
+
+    /*Create anew style*/
+    static lv_style_t style_txt;
+    lv_style_copy(&style_txt, &lv_style_plain);
+    style_txt.text.letter_space = 2;
+    style_txt.text.line_space = 1;
+    style_txt.text.color = LV_COLOR_HEX(0x606060);
+
+    lv_obj_t * txt = lv_label_create(lv_scr_act(), NULL);
+    lv_obj_set_style(txt, &style_txt);                    /*Set the created style*/
+    lv_label_set_long_mode(txt, LV_LABEL_LONG_BREAK);     /*Break the long lines*/
+    lv_label_set_recolor(txt, true);                      /*Enable re-coloring by commands in the text*/
+    lv_label_set_align(txt, LV_LABEL_ALIGN_CENTER);       /*Center aligned lines*/
+    lv_label_set_text(txt, "This is a ST7789V IPS module\n"
+                           "Together with ESP32\n"
+                           "Running LittlevGL\n"
+                           "Ported by Jackson Hu\n");
+    lv_obj_set_width(txt, 240);                           /*Set a width*/
+    lv_obj_align(txt, NULL, LV_ALIGN_CENTER, 0, 20);      /*Align to center*/
 }
 
