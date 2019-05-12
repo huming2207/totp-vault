5986fc8b5e7066790f497ce76ecf093e04b27d32
Jackson Ming Hu <huming2207@gmail.com>
2019-05-12 21:10:05 +1000

    fp_verify: implement view

--- main/view/include/fp_verify_view.hpp
+++ main/view/include/fp_verify_view.hpp
@@ -1,2 +1,17 @@
 #pragma once
 
+#include "mapped_view.hpp"
+
+class fp_verify_view : public mapped_view
+{
+    public:
+        fp_verify_view();
+        void set_icon(const char *icon);
+        void set_title_text(const char *text);
+
+    private:
+        lvgl_obj_ptr view_render_title();
+        lvgl_obj_ptr view_render_icon();
+
+        lvgl_obj_map obj_map{};
+};
