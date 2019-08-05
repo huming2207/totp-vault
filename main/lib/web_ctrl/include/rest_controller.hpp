ae0d945505968ef4e2381a2a4d167328d388ab06
Jackson Ming Hu <huming2207@gmail.com>
2019-08-05 21:55:53 +1000

    rest_controller: implement status endpoint

--- main/lib/web_ctrl/include/rest_controller.hpp
+++ main/lib/web_ctrl/include/rest_controller.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <esp_err.h>
 
 #include <esp_web_serv.hpp>
+#include <esp_err.h>
+
 
 namespace web_ctrl
 {
@@ -12,8 +13,9 @@ namespace web_ctrl
             rest_controller();
 
         private:
+            net_ctrl::esp_web_serv server;
             esp_err_t on_wifi_set(httpd_req_t *req);
-            esp_err_t on_wifi_get(httpd_req_t *req);
+            esp_err_t on_status_get(httpd_req_t *req);
             esp_err_t on_totp_set(httpd_req_t *req);
             esp_err_t on_totp_get(httpd_req_t *req);
             esp_err_t on_fp_set(httpd_req_t *req);
--- main/lib/web_ctrl/rest_controller.cpp
+++ main/lib/web_ctrl/rest_controller.cpp
@@ -1,15 +1,35 @@
+#include <ArduinoJson.h>
+#include <wifi_manager.hpp>
+
 #include "rest_controller.hpp"
 
 using namespace web_ctrl;
+using namespace net_ctrl;
 
 esp_err_t rest_controller::on_wifi_set(httpd_req_t *req)
 {
     return 0;
 }
 
-esp_err_t rest_controller::on_wifi_get(httpd_req_t *req)
+esp_err_t rest_controller::on_status_get(httpd_req_t *req)
 {
-    return 0;
+    auto &wifi_mgr = net_ctrl::wifi_manager::get_manager();
+    StaticJsonDocument<256> json_buf;
+
+    wifi_config_t config;
+    if(wifi_mgr.get_sta_config(config) != ESP_OK) {
+        esp_web_serv::set_status(req, HTTPD_404);
+        esp_web_serv::send_body(req, "");
+    }
+
+    json_buf["ssid"] = config.sta.ssid;
+    json_buf["sdk_ver"] = esp_get_idf_version();
+    json_buf["free_heap"] = esp_get_free_heap_size();
+    char json_str[128] = { 0 };
+    serializeJson(json_buf, json_str);
+    esp_web_serv::set_type(req, HTTPD_TYPE_JSON);
+    esp_web_serv::send_body(req, json_str);
+    return ESP_OK;
 }
 
 esp_err_t rest_controller::on_totp_set(httpd_req_t *req)
@@ -34,5 +54,5 @@ esp_err_t rest_controller::on_fp_get(httpd_req_t *req)
 
 rest_controller::rest_controller()
 {
-
+    server.on("/status", HTTP_GET, [this](httpd_req_t *req){ return on_status_get(req); });
 }
