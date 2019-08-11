51368eb91d73c7dca882290cb89d9b98228e3a36
Jackson Ming Hu <huming2207@gmail.com>
2019-08-11 10:26:56 +1000

    wifi_mac: decouple from wifi_manager

--- main/lib/sys/include/wifi_mac.hpp
+++ main/lib/sys/include/wifi_mac.hpp
@@ -1,18 +1,103 @@
 #pragma once
 
+#include <array>
+#include <vector>
+#include <cstdint>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/event_groups.h>
+#include <esp_wifi.h>
+#include <esp_err.h>
+
+#define WIFI_MAC_BROADCAST_ADDR { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff }
+#define WIFI_MAC_DEFAULT_OUI    { 0x18, 0xfe, 0x34 }
+
+#define WIFI_MAC_ACTION_STARTED BIT0
+
 namespace sys
 {
+    namespace def
+    {
+        enum device_config_state {
+            DEVICE_CONFIGURED       = 0,
+            DEVICE_INIT_BRAND_NEW   = 1,
+            DEVICE_INIT_RESET       = 2
+        };
+
+        enum wifi_frame_type {
+            FRAME_CTRL_MGMT_ACTION                   = 0xD0,
+            FRAME_CTRL_MGMT_BEACON                   = 0x80,
+            FRAME_CTRL_MGMT_PROBE_RESPONSE           = 0x50,
+            FRAME_CTRL_MGMT_PROBE_REQUEST            = 0x40,
+            FRAME_CTRL_MGMT_REASSOC_REQUEST          = 0x20,
+            FRAME_CTRL_MGMT_REASSOC_RESPONSE         = 0x30,
+            FRAME_CTRL_MGMT_ASSOCIATE_REQUEST        = 0x00,
+            FRAME_CTRL_MGMT_ASSOCIATE_RESPONSE       = 0x10
+        };
+
+        typedef struct __attribute__((packed)) {
+            uint8_t element_id:8;      /**< Should be set to WIFI_VENDOR_IE_ELEMENT_ID (0xDD) */
+            uint8_t length:8;          /**< Length of all bytes in the element data following this field. Minimum 4. */
+            uint8_t vendor_oui[3];   /**< Vendor identifier (OUI). */
+            uint8_t vendor_oui_type:8; /**< Vendor-specific OUI type. */
+            uint8_t mac[6];
+            uint8_t idf_ver[32];
+            device_config_state state:8;
+        } smoke_signal;
+
+        typedef struct __attribute__((packed)) {
+            union __attribute__((packed)) {
+                struct __attribute__((packed)) {
+                    wifi_frame_type frame_type:8;
+                    uint8_t frame_flag;
+                    uint16_t duration:16;
+                    uint8_t receiver_mac[6];
+                    uint8_t transmitter_mac[6];
+                    uint8_t bssid[6];
+                    uint16_t seq_frag:16;
+                };
+                uint8_t val[24];
+            };
+        } mgmt_frame_header;
+    }
+
     class wifi_mac
     {
+        // Singleton related
         private:
             wifi_mac();
 
+        // Private members
+        private:
+            std::array<uint8_t, 6> rx_addr = WIFI_MAC_BROADCAST_ADDR;
+            std::array<uint8_t, 6> tx_addr;
+            std::array<uint8_t, 6> bssid_addr = WIFI_MAC_BROADCAST_ADDR;
+            std::array<uint8_t, 3> oui = WIFI_MAC_DEFAULT_OUI;
+            esp_interface_t curr_interface = ESP_IF_WIFI_AP;
+            std::vector<uint8_t> action_payload;
+            static EventGroupHandle_t wifi_mac_event;
+
+        // Singleton related
         public:
             wifi_mac(wifi_mac const&) = delete;
             void operator=(wifi_mac const&) = delete;
             static wifi_mac& get_instance();
 
+        // Control APIs
+        public:
+            esp_err_t set_smoke_signal(def::device_config_state state);
+            esp_err_t unset_smoke_signal();
+            wifi_mac& start_send_action_frame(esp_interface_t interface, const std::vector<uint8_t> &payload);
+            wifi_mac& stop_send_action_frame();
 
+        // Setter & builders
+        public:
+            wifi_mac& set_receiver_addr(const std::array<uint8_t, 6>& _rx_addr);
+            wifi_mac& set_transmitter_addr(const std::array<uint8_t, 6>& _tx_addr);
+            wifi_mac& set_bssid_addr(const std::array<uint8_t, 6>& _bssid_addr);
+            wifi_mac& set_oui(const std::array<uint8_t, 3>& _oui);
+            wifi_mac& set_action_payload(const std::vector<uint8_t>& payload);
+            wifi_mac& set_wifi_interface(esp_interface_t interface);
     };
 }
 
--- main/lib/sys/wifi_mac.cpp
+++ main/lib/sys/wifi_mac.cpp
@@ -1,14 +1,171 @@
+#include <cstring>
+#include <algorithm>
+
+#include <esp_log.h>
+
 #include "wifi_mac.hpp"
+
+#define TAG "wifi_mac"
 
 using namespace sys;
 
+EventGroupHandle_t wifi_mac::wifi_mac_event = nullptr;
+
 wifi_mac::wifi_mac()
 {
+    // Initialise event group
+    wifi_mac_event = xEventGroupCreate();
+    if(wifi_mac_event == nullptr) abort();
 
+    // Initialise transmitter addr
+    uint8_t default_addr[6] = {0 };
+    esp_efuse_mac_get_default(default_addr);
+
+    tx_addr = { MAC2STR(default_addr) };
 }
 
 wifi_mac& wifi_mac::get_instance()
 {
     static wifi_mac mac;
     return mac;
+}
+
+esp_err_t wifi_mac::set_smoke_signal(def::device_config_state state)
+{
+    // Construct vendor IE
+    size_t idf_ver_len = strlen(esp_get_idf_version()) + 1;
+
+    def::smoke_signal payload{};
+    payload.element_id = WIFI_VENDOR_IE_ELEMENT_ID;
+    payload.length = sizeof(def::smoke_signal);
+    payload.vendor_oui_type = 2;
+    std::memcpy(payload.vendor_oui, oui.data(), oui.size());
+    esp_efuse_mac_get_default(payload.mac);
+    std::memcpy(payload.idf_ver, esp_get_idf_version(), std::min(idf_ver_len, (size_t)32));
+    payload.state = state;
+
+    // Register payload
+    auto ret = esp_wifi_set_vendor_ie(true, WIFI_VND_IE_TYPE_BEACON, WIFI_VND_IE_ID_0, &payload);
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to set vendor IE, ret: %u", ret);
+        return ret;
+    }
+
+    return ret;
+}
+
+esp_err_t wifi_mac::unset_smoke_signal()
+{
+    return 0;
+}
+
+wifi_mac& wifi_mac::start_send_action_frame(esp_interface_t interface, const std::vector<uint8_t> &payload)
+{
+    xEventGroupSetBits(wifi_mac_event, WIFI_MAC_ACTION_STARTED);
+
+    xTaskCreate([](void *ptr){
+
+        auto *mac_class = static_cast<wifi_mac *>(ptr);
+
+        def::mgmt_frame_header header;
+
+        // Initialise action header
+        header.frame_type = def::FRAME_CTRL_MGMT_ACTION;
+        header.frame_flag = 0;
+        header.duration = 0;
+        header.seq_frag = 0; // Placeholder for now, the WiFi PHY will fill in the correct value by itself
+        std::memcpy(header.receiver_mac, mac_class->rx_addr.data(), mac_class->rx_addr.size());
+        std::memcpy(header.transmitter_mac, mac_class->tx_addr.data(), mac_class->tx_addr.size());
+        std::memcpy(header.bssid, mac_class->bssid_addr.data(), mac_class->bssid_addr.size());
+
+        std::vector<uint8_t> frame;
+
+        // Insert Action frame header
+        frame.insert(frame.end(), header.val, header.val + sizeof(def::mgmt_frame_header));
+
+        // Append Action frame type
+        frame.emplace_back(0x7f); // Vendor-specific Action frame
+
+        // Append OUI
+        frame.insert(frame.end(),
+                     std::make_move_iterator(mac_class->oui.begin()),
+                     std::make_move_iterator(mac_class->oui.end()));
+
+        // Append actual data payload
+        frame.insert(frame.end(),
+                     std::make_move_iterator(mac_class->action_payload.begin()),
+                     std::make_move_iterator(mac_class->action_payload.end()));
+
+        auto ret = ESP_OK;
+        while((xEventGroupGetBits(wifi_mac_event) & WIFI_MAC_ACTION_STARTED) == WIFI_MAC_ACTION_STARTED) {
+            ret = esp_wifi_80211_tx(mac_class->curr_interface, frame.data(), frame.size(), true);
+            if(ret != ESP_OK) {
+                ESP_LOGE(TAG, "Error occured when sending action frame: %d, now quitting...", ret);
+                xEventGroupClearBits(wifi_mac_event, WIFI_MAC_ACTION_STARTED);
+            }
+
+            vTaskDelay(pdMS_TO_TICKS(150));
+        }
+
+        vTaskDelete(nullptr); // Kill itself if signal is gone
+    }, "mac_action", 2048, this, tskIDLE_PRIORITY + 5, nullptr);
+
+    // Send off the buffer with repeating
+    return *this;
+}
+
+wifi_mac& wifi_mac::stop_send_action_frame()
+{
+    // Ignore request if the action frame sender thread has started
+    if((xEventGroupGetBits(wifi_mac_event) & WIFI_MAC_ACTION_STARTED) == WIFI_MAC_ACTION_STARTED) return *this;
+    xEventGroupClearBits(wifi_mac_event, WIFI_MAC_ACTION_STARTED);
+    return *this;
+}
+
+wifi_mac &wifi_mac::set_receiver_addr(const std::array<uint8_t, 6> &_rx_addr)
+{
+    // Ignore setting request if the action frame sender thread has started
+    if((xEventGroupGetBits(wifi_mac_event) & WIFI_MAC_ACTION_STARTED) == WIFI_MAC_ACTION_STARTED) return *this;
+    rx_addr = _rx_addr;
+    return *this;
+}
+
+wifi_mac &wifi_mac::set_transmitter_addr(const std::array<uint8_t, 6> &_tx_addr)
+{
+    // Ignore setting request if the action frame sender thread has started
+    if((xEventGroupGetBits(wifi_mac_event) & WIFI_MAC_ACTION_STARTED) == WIFI_MAC_ACTION_STARTED) return *this;
+    tx_addr = _tx_addr;
+    return *this;
+}
+
+wifi_mac &wifi_mac::set_bssid_addr(const std::array<uint8_t, 6> &_bssid_addr)
+{
+    // Ignore setting request if the action frame sender thread has started
+    if((xEventGroupGetBits(wifi_mac_event) & WIFI_MAC_ACTION_STARTED) == WIFI_MAC_ACTION_STARTED) return *this;
+    bssid_addr = _bssid_addr;
+    return *this;
+}
+
+wifi_mac &wifi_mac::set_oui(const std::array<uint8_t, 3> &_oui)
+{
+    // Ignore setting request if the action frame sender thread has started
+    if((xEventGroupGetBits(wifi_mac_event) & WIFI_MAC_ACTION_STARTED) == WIFI_MAC_ACTION_STARTED) return *this;
+    oui = _oui;
+    return *this;
+}
+
+wifi_mac &wifi_mac::set_action_payload(const std::vector<uint8_t> &payload)
+{
+    // Ignore setting request if the action frame sender thread has started
+    if((xEventGroupGetBits(wifi_mac_event) & WIFI_MAC_ACTION_STARTED) == WIFI_MAC_ACTION_STARTED) return *this;
+    action_payload = payload;
+    return *this;
+}
+
+wifi_mac &wifi_mac::set_wifi_interface(esp_interface_t interface)
+{
+    // Ignore setting request if the action frame sender thread has started
+    if((xEventGroupGetBits(wifi_mac_event) & WIFI_MAC_ACTION_STARTED) == WIFI_MAC_ACTION_STARTED) return *this;
+    curr_interface = interface;
+    return *this;
 }
