84190a16bca65ab90e623684f5765ea83bbb0519
Jackson Ming Hu <huming2207@gmail.com>
2019-08-21 21:37:35 +1000

    Drop Fingerprint UI

--- main/lvgl_main.cpp
+++ main/lvgl_main.cpp
@@ -7,6 +7,7 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <cstdio>
+#include <cstring>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -14,18 +15,15 @@
 #include <esp_freertos_hooks.h>
 #include <esp_log.h>
 #include <lv_core/lv_style.h>
-#include <totp_controller.hpp>
 
-#include "otp.h"
-#include "fp1020a.h"
 #include "lvgl.h"
 #include "lvgl_hal_st7789.h"
 
-#include "fp_add_controller.hpp"
-#include "fp_verify_controller.hpp"
-
 #include <wifi_manager.hpp>
 #include <rest_controller.hpp>
+
+#include <esp_now.h>
+#include "lvgl_hal_st7735r.h"
 
 #define TAG "esp-lvgl"
 
@@ -36,7 +34,7 @@ extern "C" {
 
 static void lvgl_main_task(void *p)
 {
-    while(1) {
+    while(true) {
         vTaskDelay(1);
         lv_task_handler();
     }
@@ -53,49 +51,36 @@ void app_main()
 {
     printf("Hello world!\n");
 
-    auto &wifi_mgr = net_ctrl::wifi_manager::get_manager();
-    ESP_ERROR_CHECK(wifi_mgr.set_sta_config("hu-home", "***huhome6789"));
-    ESP_ERROR_CHECK(wifi_mgr.set_ap_config("Yeet", "test123456", 3));
-    ESP_ERROR_CHECK(wifi_mgr.start(WIFI_MODE_APSTA));
-    auto rest_mgr = web_ctrl::rest_controller();
+//    ESP_LOGI(TAG, "Initialising NVS");
+//    nv_storage::flash_init();
+
+    ESP_LOGI(TAG, "Initialising LittlevGL");
+    lv_init();
+
+    ESP_LOGI(TAG, "Initialising ST7789");
+    lvgl_st7735r_init();
+
+    ESP_LOGI(TAG, "Initialising LVGL driver");
+    static lv_color_t buf1[ST7735R_BUF_SIZE];
+    static lv_color_t buf2[ST7735R_BUF_SIZE];
+    static lv_disp_buf_t disp_buf;
+    lv_disp_buf_init(&disp_buf, buf1, buf2, ST7735R_BUF_SIZE);
+
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.buffer = &disp_buf;
+    disp_drv.flush_cb = lvgl_st7735r_flush_cb;
+
+    ESP_LOGI(TAG, "Registering LVGL driver");
+    lv_disp_drv_register(&disp_drv);
+
+
+    ESP_LOGI(TAG, "Registering LVGL tick callback");
+    esp_register_freertos_tick_hook(lv_tick_cb);
+
+    ESP_LOGI(TAG, "Starting LVGL main task");
+    xTaskCreate(lvgl_main_task, "lv_task", 8192, nullptr, 5, nullptr);
+
     vTaskDelay(portMAX_DELAY);
-
-////    ESP_LOGI(TAG, "Initialising NVS");
-////    nv_storage::flash_init();
-//
-//    ESP_LOGI(TAG, "Initialising FP1020");
-//    fp1020a_init();
-//    fp1020a_remove_all_user();
-//
-//    ESP_LOGI(TAG, "Initialising LittlevGL");
-//    lv_init();
-//
-//    ESP_LOGI(TAG, "Initialising ST7789");
-//    lvgl_st7789_init();
-//
-//    ESP_LOGI(TAG, "Initialising LVGL driver");
-//    lv_disp_drv_t disp;
-//    lv_disp_drv_init(&disp);
-//    disp.flush_cb = lvgl_st7789_flush;
-//
-//    ESP_LOGI(TAG, "Registering LVGL driver");
-//    lv_disp_drv_register(&disp);
-//
-//
-//    ESP_LOGI(TAG, "Registering LVGL tick callback");
-//    esp_register_freertos_tick_hook(lv_tick_cb);
-//
-//    ESP_LOGI(TAG, "Starting LVGL main task");
-//    xTaskCreate(lvgl_main_task, "lv_task", 8192, nullptr, 5, nullptr);
-//
-//    fp_add_controller add_controller;
-//    add_controller.preform_enroll();
-//    vTaskDelay(pdMS_TO_TICKS(5000));
-//    add_controller.~fp_add_controller();
-//
-//    fp_verify_controller verify_controller;
-//    verify_controller.verify_fp();
-//    vTaskDelay(portMAX_DELAY);
-
 }
 
