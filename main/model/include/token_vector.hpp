7c0ed635db96c26f3b860235d853d753249eb60a
Jackson Ming Hu <huming2207@gmail.com>
2019-09-08 11:02:00 +1000

    token_vector: singleton-ise

--- main/model/include/token_vector.hpp
+++ main/model/include/token_vector.hpp
@@ -10,11 +10,20 @@ class token_vector : private std::vector<otp_key>
     public:
         using vector::emplace_back;
         using vector::operator[];
+        using vector::at;
+        using vector::shrink_to_fit;
         using vector::begin;
         using vector::end;
-        token_vector();
+        static token_vector& instance()
+        {
+            static token_vector vec;
+            return vec;
+        };
+        token_vector(token_vector const&) = delete;
+        void operator=(token_vector const&) = delete;
         esp_err_t save();
     private:
+        token_vector();
         nvs_handle_t handle = 0;
 };
 
