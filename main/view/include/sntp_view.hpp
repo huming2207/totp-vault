6d2117203f1f2d746710308caa348859f9303731
Jackson Ming Hu <huming2207@gmail.com>
2019-09-22 15:15:18 +1000

    sntp_view: initial implementation

--- main/view/include/sntp_view.hpp
+++ main/view/include/sntp_view.hpp
@@ -1,6 +1,22 @@
 #pragma once
 
+#include <lvgl.h>
+
 class sntp_view
 {
+    public:
+        sntp_view();
+        ~sntp_view();
 
+        void set_sync_text();
+        void set_connect_text();
+        void set_done_text();
+        void set_wifi_error_text();
+        void set_sync_error_text();
+
+    private:
+
+    private:
+        lv_obj_t* status_label = nullptr;
+        lv_obj_t* icon_label = nullptr;
 };
--- main/view/sntp_view.cpp
+++ main/view/sntp_view.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "sntp_view.hpp"
+
+sntp_view::sntp_view()
+{
+    status_label = lv_label_create(lv_scr_act(), nullptr);
+    lv_label_set_long_mode(status_label, LV_LABEL_LONG_BREAK);
+    lv_label_set_text(status_label, "Unknown status");
+    lv_obj_set_width(status_label, 125);
+    lv_obj_align(status_label, nullptr, LV_ALIGN_CENTER, 15, -10);
+
+    icon_label = lv_label_create(lv_scr_act(), nullptr);
+    lv_label_set_text(icon_label, LV_SYMBOL_WARNING);
+    lv_obj_align(icon_label, nullptr, LV_ALIGN_IN_LEFT_MID, 10, 0);
+}
+
+void sntp_view::set_connect_text()
+{
+    lv_label_set_text(status_label, "Waiting for\nWiFi connection");
+    lv_label_set_text(icon_label, LV_SYMBOL_WIFI);
+}
+
+void sntp_view::set_sync_text()
+{
+    lv_label_set_text(status_label, "Syncing time\nfrom network...");
+    lv_label_set_text(icon_label, LV_SYMBOL_REFRESH);
+}
+
+
+void sntp_view::set_done_text()
+{
+    lv_label_set_text(status_label, "Done!\nreboot in 5 sec...");
+    lv_label_set_text(icon_label, LV_SYMBOL_OK);
+}
+
+void sntp_view::set_wifi_error_text()
+{
+    lv_label_set_text(status_label, "Failed!\nNo WiFi AP");
+    lv_label_set_text(icon_label, LV_SYMBOL_CLOSE);
+}
+
+void sntp_view::set_sync_error_text()
+{
+    lv_label_set_text(status_label, "Failed!\nSync failed");
+    lv_label_set_text(icon_label, LV_SYMBOL_CLOSE);
+}
+
+sntp_view::~sntp_view()
+{
+    lv_obj_del(status_label);
+    lv_obj_del(icon_label);
+}
