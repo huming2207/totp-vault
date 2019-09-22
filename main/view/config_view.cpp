e7e66a1e6bea4d67d4937931e8af7490af5008dd
Jackson Ming Hu <huming2207@gmail.com>
2019-09-22 17:28:55 +1000

    config_view: init impl

--- main/view/config_view.cpp
+++ main/view/config_view.cpp
@@ -1,5 +1,19 @@
-//
-// Created by hu on 15/9/19.
-//
-
 #include "config_view.hpp"
+
+config_view::config_view(const std::string& ssid)
+{
+    main_label = lv_label_create(lv_scr_act(), nullptr);
+    lv_label_set_long_mode(main_label, LV_LABEL_LONG_BREAK);
+    lv_label_set_align(main_label, LV_LABEL_ALIGN_LEFT);
+    lv_label_set_recolor(main_label, true);
+    lv_obj_set_width(main_label, 130);
+    lv_obj_align(main_label, nullptr, LV_ALIGN_CENTER, 0, -20);
+
+    std::string label_txt = "Please connect to\nWiFi AP:\n#ff0000 " + ssid + '#';
+    lv_label_set_text(main_label, label_txt.c_str());
+}
+
+config_view::~config_view()
+{
+    lv_obj_del(main_label);
+}
--- main/view/include/config_view.hpp
+++ main/view/include/config_view.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <lvgl.h>
+#include <string>
+
 class config_view
 {
+    public:
+        config_view(const std::string& ssid);
+        ~config_view();
 
+    private:
+        lv_obj_t* main_label = nullptr;
 };
 
